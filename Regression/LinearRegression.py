672d0fce2c06ad855f8ff1ab95b80db34ec03d57
wyp <1140664142@qq.com>
2015-04-28 02:13:49 +0800

    add module linear regresion

---
+++ Regression/LinearRegression.py
@@ -0,0 +1,25 @@
+def f(TrainingMatrix, TrainingResult, TestMatrix):
+  theta = []
+  for i in range(Size):
+    theta.append(0)
+  loss = 1000.0
+  rate = 0.01
+  RowSize = len(TrainingMatrix)
+  ColSize = len(TrainingMatrix[0])
+
+  for i in range(100):
+    if loss < 0.0001:
+      break
+    error_sum = 0.0
+    for j in range(RowSize):
+      h = 0.0
+      for k in range(ColSize):
+        h += TrainingMatrix[j][k] * theta[k]
+      error_sum = TrainingResult[j] - h
+      for k in range(ColSize):
+        theta[k] += rate * error_sum * TrainingMatrix[j][k]
+
+  ans = []
+  
+  return ans
+
---
+++ Regression/__init__.py
@@ -0,0 +1,4 @@
+# Author : banehallow
+# Email  : 1140664142@qq.com
+
+from PackageName import *
